4f5975c4536b35f3553b70a25efe118ebd770717
Adam Nordquist <apnordquist@gmail.com>
2024-01-24 08:04:15 -0700

    Initialization

---
+++ Lab1/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Person
+    {
+        //attributes
+        public int id;
+        public string firstName;
+        public string lastName;
+        public string favouriteColour;
+        public int age;
+        public bool isWorking;
+        //constructor
+        public Person(int pId, string first, string last, string colour, int ageYrs, bool working)
+        {
+            id = pId;
+            firstName = first;
+            lastName = last;
+            favouriteColour = colour;
+            age = ageYrs;
+            isWorking = working;
+        }
+        //methods
+        public void DisplayPersonInfo(Person p)
+        {
+            Console.WriteLine(p.firstName p.lastName);
+        }
+        public void PersonId(Person p)
+        {
+            Console.WriteLine(p.id);
+        }
+        public void ChangeFavouritColour(string newColour)
+        {
+            favouriteColour = newColour;
+        }
+        public void GetAgeInTenYears(Person p)
+        {
+            Console.Write(p.age + 10);
+        }
+        public void ToString(Person p) //display all information as a list
+        {
+            Console.WriteLine();
+        }
+     }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ian = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person gina = new Person(2, "Gina", "James", "Green", 18, false);
+            Person mike = new Person(3, "Mike", "Ben", "Blue", 45, true);
+            Person mary = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+        }
+    }
+}
